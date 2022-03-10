db07fab64b76e025600cdaa07d53c5567e262606
yaroslav_zh <yaroslavne@yahoo.com>
2022-03-10 21:56:17 +0300

    Start build Covid tracker

---
+++ covid_tracker/covid.demo/src/test/java/online/zhimanov/covid/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package online.zhimanov.covid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
